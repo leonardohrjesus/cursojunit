f89763e1e0a1d6fb440bb9244e8a676837befd74
Leonardo <leonardo.hrjesus@hotmail.com>
2020-10-01 22:53:21 -0300

    projeto inicial

---
+++ library-api/src/test/java/com/cursoteste/libraryapi/LibraryApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cursoteste.libraryapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
